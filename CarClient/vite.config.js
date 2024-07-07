5101b363b0eafb9a47d936cce6e96126ceea119e
Serdar Özdemir <soezdemir3423@gmail.com>
2024-07-07 11:18:42 +0200

    feat: Add proxy configuration for API requests in Vite server

--- CarClient/vite.config.js
+++ CarClient/vite.config.js
@@ -6,6 +6,24 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // Client port
-    host: true
+    proxy: {
+      '/cars': {
+        target: 'http://localhost:5059',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Requeset to the Target:', req.method, req.url)
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target: ', proxyRes.statusCode, req.url)
+          })
+        }
+      }
+    }
   }
 })
