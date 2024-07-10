9f28a6f8ffe8b8eb2f68c8f86c7d53b84c360923
Serdar Özdemir <soezdemir3423@gmail.com>
2024-07-10 16:31:19 +0200

    feat: Update CarApi to handle CORS policy for specific origins

--- CarApi/Program.cs
+++ CarApi/Program.cs
@@ -5,7 +5,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 //Json
-builder.Services.ConfigureHttpJsonOptions(options => {
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
     options.SerializerOptions.WriteIndented = true;
     options.SerializerOptions.IncludeFields = true;
 });
@@ -22,11 +23,15 @@ builder.Services.AddSqlite<CarDb>(connectionString);
 // 1) define a unique string
 string MyAllowSpecificOrigins = "_myallowSpecificOrigins";
 
-// 2) deine allowed domains, in this case "http://cars-db.com" and "*" = all domains, for testing purposes only
- builder.Services.AddCors(options =>
- {
-    options.AddPolicy(name: MyAllowSpecificOrigins, builder => {builder.WithOrigins("http://cars-db.com", "*");});
- });
+// 2) define allowed domains, in this case "http://cars-db.com" and "*" = all domains, for testing purposes only
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, builder => 
+    {
+         builder.WithOrigins("http://cars-db.com", "*"); 
+    });
+});
+
 //Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
@@ -49,16 +54,18 @@ if (app.Environment.IsDevelopment())
         config.DocExpansion = "list";
     });
 }
+// use the cors capability 
+app.UseCors(MyAllowSpecificOrigins);
 
 // GroupMapping API
-var carItems = app.MapGroup("/caritems");
+var carItems = app.MapGroup("/cars");
 
 carItems.MapGet("/", GetAllCars);
 carItems.MapGet("/reserved", GetReservedCars);
 carItems.MapGet("/{id}", GetCarById);
 carItems.MapPost("/", CreateNewCar);
 carItems.MapPut("/{id}", UpdateCar);
-carItems.MapDelete("/{id}",RemoveCarById);
+carItems.MapDelete("/{id}", RemoveCarById);
 
 static async Task<IResult> GetAllCars(CarDb db)
     => TypedResults.Ok
@@ -67,33 +74,37 @@ static async Task<IResult> GetAllCars(CarDb db)
         .ToArrayAsync()
     );
 
-static async Task<IResult> GetReservedCars(CarDb db) {
+static async Task<IResult> GetReservedCars(CarDb db)
+{
     List<Car> cars = await db.Cars.Where(c => c.Reserved == true).ToListAsync();
     List<Car> reservedCars = cars.Where(car => car.Reserved).ToList();
-    return reservedCars.Count > 1 ? TypedResults.Ok(reservedCars): TypedResults.NotFound("All cars are freely available");
-} 
+    return reservedCars.Count > 1 ? TypedResults.Ok(reservedCars) : TypedResults.NotFound("All cars are freely available");
+}
 
 static async Task<IResult> GetCarById(int id, CarDb db)
 {
-    return await db.Cars.FindAsync(id) 
-        is Car car 
-        ? TypedResults.Ok(new CarDTO(car)) 
+    return await db.Cars.FindAsync(id)
+        is Car car
+        ? TypedResults.Ok(new CarDTO(car))
         : TypedResults.NotFound("No Such car found");
 }
 
-static async Task<IResult> CreateNewCar(Car car, CarDb db){
+static async Task<IResult> CreateNewCar(Car car, CarDb db)
+{
     await db.Cars.AddAsync(car);
     await db.SaveChangesAsync();
     CarDTO itemDTO = new CarDTO(car);
-    return  TypedResults.Created($"/caritems/{car.Id}", itemDTO);
+    return TypedResults.Created($"/caritems/{car.Id}", itemDTO);
 }
 
 
-static async Task<IResult> UpdateCar(int id, CarDTO inputCar, CarDb db){
+static async Task<IResult> UpdateCar(int id, CarDTO inputCar, CarDb db)
+{
     Car? car = await db.Cars.FindAsync(id);
 
     if (car is null) return TypedResults.NotFound($"The specificied car with the id {id} is not found: {car}");
-    else {
+    else
+    {
         car.Brand = inputCar.Brand;
         car.Model = inputCar.Model;
         car.Price = inputCar.Price;
@@ -105,12 +116,15 @@ static async Task<IResult> UpdateCar(int id, CarDTO inputCar, CarDb db){
     return TypedResults.Ok($"Updates are succesful:\n {car}");
 }
 
-static async Task<IResult> RemoveCarById(int id, CarDb db){
-    if (await db.Cars.FindAsync(id) is Car car){
+static async Task<IResult> RemoveCarById(int id, CarDb db)
+{
+    if (await db.Cars.FindAsync(id) is Car car)
+    {
         db.Cars.Remove(car);
         await db.SaveChangesAsync();
         return TypedResults.Ok("Removed");
-    } return TypedResults.NotFound($"No such car found with id {id}.");
+    }
+    return TypedResults.NotFound($"No such car found with id {id}.");
 }
 
 app.Run();
