d79265e0aff4e8dc99f5aeadb9b9343a699eeb59
Serdar Özdemir <soezdemir3423@gmail.com>
2024-07-10 16:30:55 +0200

    feat: Update proxy configuration for API requests in Vite server

--- CarClient/vite.config.js
+++ CarClient/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000, // Client port
     proxy: {
       '/cars': {
-        target: 'http://localhost:5100',// mock server port
+        target: 'http://localhost:5049',// mock server port
         changeOrigin: true,
         secure: false,
         ws: true,
