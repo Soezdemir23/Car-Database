67a60bf395ec4984150e02e4544be6476de47b1f
Serdar Özdemir <soezdemir3423@gmail.com>
2024-07-16 22:32:03 +0200

    refactor: Update car component to include form input handling and CRUD functionality

--- Car-Frontend/src/app/car/car.component.ts
+++ Car-Frontend/src/app/car/car.component.ts
@@ -2,9 +2,8 @@ import { Component, Injectable, Input, OnInit } from '@angular/core';
 import { CarInterface } from '../car-interface';
 import { CarListComponent } from '../car-list/car-list.component';
 import { DatePipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { CarServiceService } from '../car-service.service';
-
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 @Component({
   selector: 'app-car',
   standalone: true,
@@ -21,7 +20,17 @@ export class CarComponent implements OnInit {
 
   ngOnInit(): void {
     this.carService.getCars().subscribe({
-      next: (data) => this.cars = data,
+      next: (data) => {
+        console.log(data)
+        const middle = data.map(car =>{
+          return {
+            ...car,
+            year: new Date(car.year).toISOString()
+          }
+        })
+        console.log("Cars on init: ", middle)
+        this.cars = middle; 
+        },
       error: (e) => console.error("an error trying to get cars: ", e),
       complete: () => console.log("complete")
     });
@@ -31,27 +40,34 @@ export class CarComponent implements OnInit {
   //fetch will be handled later when the api part is set up
 
   handleSubmitCar($event: CarInterface) {
+    console.log("Befor update date: ", $event.year);
+    $event.year = new Date($event.year);
+    console.log("After update date: ", $event.year)
     if ($event.id > 0) {
       this.carService.updateCar($event, $event.id).subscribe({
         next: (updatedCar) => {
           console.log("Car updated: " + JSON.stringify(updatedCar))
           const index = this.cars.findIndex(car => car.id === updatedCar.id);
-          if (index !== -1) this.cars[index] = updatedCar;
+          console.log("index of car that is updated: ", updatedCar)
+          this.cars = this.cars.map(car => car.id === index? updatedCar: car)
+          if (index !== -1) {
+         
+          }
         },
         error: (e) => console.error("An error during update: ", e),
         complete: () => "update finished"
       })
     } else {
+      $event.id = this.cars.length + 1;
       this.carService.createCar($event).subscribe({
         next: (newCar) => {
-          newCar.id = this.cars.length + 1
           console.log("Car created: ", newCar)
-          this.cars.push(newCar)
+          //adding a new car, making angular re-render the new entry
+          this.cars = [...this.cars, newCar];
         }
         ,
         error: (e) => console.error("Error creating car: ", e),
         complete: () => console.log("car creation finished")
-
       })
     }
 
