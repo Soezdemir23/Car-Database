d1f884077ca5644efe9facce80743cf3675721dd
Serdar Özdemir <soezdemir3423@gmail.com>
2024-08-10 02:24:39 +0200

    chore (/docs): add errorhandling to the carr service in the app and built it to test it.

--- Car-Frontend/src/app/car-service.service.ts
+++ Car-Frontend/src/app/car-service.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CarInterface } from './car-interface';
-
+import { catchError } from 'rxjs/operators';
+import {throwError} from 'rxjs'; 
 @Injectable({
   providedIn: 'root'
   
@@ -12,25 +13,41 @@ export class CarServiceService {
 
   constructor(private http: HttpClient) {}
   
-  getCars(){
-    return this.http.get<CarInterface[]>("https://cars-database.tryasp.net/cars");
+  getCars() {
+    return this.http.get<CarInterface[]>("https://cars-database.tryasp.net/cars").pipe(
+      catchError(this.handleError)
+    );
   }
 
   getCarById(id:number){
-    return this.http.get<CarInterface>(`https://cars-database.tryasp.net/cars/${id}`);
+    return this.http.get<CarInterface>(`https://cars-database.tryasp.net/cars/${id}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
   updateCar(newCar: CarInterface, id: number){
-    return this.http.put<CarInterface>(`https://cars-database.tryasp.net/cars/${id}`, newCar);
+    return this.http.put<CarInterface>(`https://cars-database.tryasp.net/cars/${id}`, newCar).pipe(
+      catchError(this.handleError)
+    );
   }
 
   createCar(newCar:CarInterface) {
-    return this.http.post<CarInterface>("https://cars-database.tryasp.net/cars", newCar);
+    return this.http.post<CarInterface>("https://cars-database.tryasp.net/cars", newCar).pipe(
+      catchError(this.handleError)
+    );
   }
 
   removeCar(id: number){
-    return this.http.delete<CarInterface>(`https://cars-database.tryasp.net/cars/${id}`);
+    return this.http.delete<CarInterface>(`https://cars-database.tryasp.net/cars/${id}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
-
+  private handleError(error: HttpErrorResponse) {
+    //handle error
+    console.error('An error occurred getting or using cars', error.error.message);
+    return throwError(
+      'Something bad happened; please try again later.'
+    )
+  }
 }
